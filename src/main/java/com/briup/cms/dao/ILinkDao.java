f97dad4d7d20802c9f170325db9272dd1c8610b0
llz <2663172168@qq.com>
2019-11-25 23:18:08 +0800

    完成链接管理功能

--- src/main/java/com/briup/cms/dao/ILinkDao.java
+++ src/main/java/com/briup/cms/dao/ILinkDao.java
@@ -1,7 +1,9 @@
 package com.briup.cms.dao;
 
 import com.briup.cms.bean.Link;
+import com.briup.cms.exception.CustomerException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ILinkDao extends JpaRepository<Link,Integer> {
+    Link queryById(int id);
 }
--- src/main/java/com/briup/cms/service/ILinkService.java
+++ src/main/java/com/briup/cms/service/ILinkService.java
@@ -3,6 +3,12 @@ package com.briup.cms.service;
 import com.briup.cms.bean.Link;
 import com.briup.cms.exception.CustomerException;
 
+import java.util.List;
+
 public interface ILinkService {
-    void addLink(Link link) throws CustomerException;
+    void saveOrUpdateLink(Link link) throws CustomerException;
+    void deleteLinkById(int id) throws CustomerException;
+    Link queryById(int id) throws CustomerException;
+    List<Link> getAllLink() throws CustomerException;
+
 }
